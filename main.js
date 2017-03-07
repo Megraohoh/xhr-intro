40b08b0e5ad0fd2fee4f0fb236a47fefe0328951
Meghan Debity <mdebity@gmail.com>
2017-03-06 19:57:35 -0600

    added code to call json details

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 
+function executeThisCodeAfterFileLoaded(){
+	console.log("IT WORKED!!!");
+}
+
+function executeThisCodeAfterFileFails(){
+	console.log("Not working, booooo");
+}
 
 
+// five lines of code and two functions
+
+var myRequest = new XMLHttpRequest(); //~new~ is a constructor
+myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+myRequest.addEventListener("error", executeThisCodeAfterFileFails);
+myRequest.open("GET", "dinosaur.json");
+myRequest.send();
+console.log("myRequest", myRequest);
+
+// GET get info out
+// POST update something 
+// PUT
+// DELETE
 
 
 
